3ff2697b6a5c21f9741713373d3590430f99ddd4
Yasmin <yasmin.alves16@outlook.com.br>
2022-08-08 17:30:02 -0300

    feat: add and delete task

--- src/components/ListCard/index.js
+++ src/components/ListCard/index.js
@@ -1,10 +1,15 @@
+import { useTasks } from "../../providers/tasks";
+
 const ListCard = ({task, id}) => {
+
+  const { removeFromList } = useTasks();
+
   return (
     <li>
       <label htmlFor={id}>{task}</label>
       <button>Completa</button>
       <button>Atualizar</button>
-      <button>Deletar</button>
+      <button onClick={() => removeFromList(id)}>Deletar</button>
     </li>
   );
 };
