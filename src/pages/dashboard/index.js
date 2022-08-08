13d5e521c25bb866207dd058e211dbdb24d42518
Yasmin <yasmin.alves16@outlook.com.br>
2022-08-08 15:48:10 -0300

    feat: input field base

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,10 +1,25 @@
-
 const dashboard = () => {
-    return(
-        <>
-        <p>Oii Dash</p>
-        </>
-    )
-}
+  return (
+    <>
+      <header>
+        <p>Lista de Tarefas</p>
+      </header>
+      <main>
+        <form autoComplete="off">
+            <input type="text" name="task" id="task" placeholder="O que temos para fazer ?" required/>
+            <button type="submit">Adicionar task</button>
+        </form>
+        <ul>
+            <li>
+                <label htmlFor="task">Criando Base</label>
+                <button>Completa</button>
+                <button>Atualizar</button>
+                <button>Deletar</button>
+            </li>
+        </ul>
+      </main>
+    </>
+  );
+};
 
-export default dashboard
+export default dashboard;
