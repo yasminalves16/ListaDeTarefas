2c5ef4538cd790d3460290b1d65be0b8a9fcc0a5
Yasmin <yasmin.alves16@outlook.com.br>
2022-08-10 03:04:27 -0300

    feat: All functions implemented

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,16 +1,27 @@
 import ListCard from "../ListCard";
 import { useTasks } from "../../providers/tasks";
+import Completed from "../../utils/completed.gif";
 
 const List = () => {
-    const { showTasks, valueButton } = useTasks();
+    const { showTasks } = useTasks();
+    console.log(showTasks);
 
     return (
         <>
-            <ul>
-                {showTasks.map((task, index) => (
-                    <ListCard key={index} task={task} id={index} value={valueButton}/>
-                ))}
-            </ul>
+            {showTasks.length > 0 ? (
+                <ul>
+                    {showTasks.map((task, index) => (
+                        <ListCard key={index} task={task} id={index} />
+                    ))}
+                </ul>
+            ) : (
+                <figure>
+                    <img
+                        src={Completed}
+                        alt="Gif comemorativo todas as tasks concluidas"
+                    />
+                </figure>
+            )}
         </>
     );
 };
--- src/components/ListCard/index.js
+++ src/components/ListCard/index.js
@@ -1,42 +1,57 @@
 import { useTasks } from "../../providers/tasks";
 import { useState } from "react";
 
+const ListCard = ({ task, id }) => {
+    const { removeFromList, completedList, updateItem, valueButton } = useTasks();
+    const [onEdit, setOnEdit] = useState(false);
+    const [editValue, setEditValue] = useState(task);
 
-const ListCard = ({ task, id, value }) => {
-  const { removeFromList, completedList, updateItem } = useTasks();
-  const [onEdit, setOnEdit] = useState(false);
-  const [editValue, setEditValue] = useState(task);
+    const handleOnEdit = () => {
+        setOnEdit(true);
+    };
 
-  const handleOnEdit = () => {
-    setOnEdit(true);
-  };
 
-  const handleSave = (id) => {
-    setOnEdit(false);
-    if (editValue) {
-      updateItem(id, editValue);
+    const handleSave = (id) => {
+        setOnEdit(false);
+        if (editValue) {
+            updateItem(id, editValue);
+        } else {
+            setEditValue(task);
+        }
+    };
+
+    if (onEdit) {
+        return (
+            <li>
+                <input
+                    type="text"
+                    id="editValue"
+                    value={editValue}
+                    name="editValue"
+                    onChange={(e) => setEditValue(e.target.value.toLowerCase())}
+                />
+                <button onClick={() => handleSave(id)}>Save</button>
+            </li>
+        );
     } else {
-      setEditValue(task);
+        if (valueButton === "Todos") {
+            return (
+                <li className={valueButton}>
+                    <label htmlFor={id}>{task}</label>
+                    <button onClick={() => completedList(task, id)}>
+                        Completa
+                    </button>
+                    <button onClick={handleOnEdit}> Atualizar </button>
+                    <button onClick={() => removeFromList(id)}>Deletar</button>
+                </li>
+            );
+        }
+        return(
+            <li className={valueButton}>
+                <label htmlFor={id}>{task}</label>
+            </li>
+        )
     }
-  };
-
-  if (onEdit) {
-    return (
-      <li>
-        <input type="text" id="editValue" value={editValue} name="editValue" onChange={(e) => setEditValue(e.target.value.toLowerCase())} />
-        <button onClick={() => handleSave(id)}>Save</button>
-      </li>
-    );
-  } else {
-    return (
-      <li className={value}>
-        <label htmlFor={id}>{task}</label>
-        <button onClick={() => completedList(task, id)}>Completa</button>
-        <button onClick={handleOnEdit}> Atualizar </button>
-        <button onClick={() => removeFromList(id)}>Deletar</button>
-      </li>
-    );
-  }
 };
 
 export default ListCard;
